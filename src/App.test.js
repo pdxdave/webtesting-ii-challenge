7a0ca511eec44479441d1d14a72c66500ed76f43
kamaboko1 <github65@hotmail.com>
2019-06-25 17:50:39 -0700

    created App.text.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'; // Need this!!!!
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe("<App />", () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('the page shows up correctly', () => {
+    render('<App />')
+  })
+
+})
